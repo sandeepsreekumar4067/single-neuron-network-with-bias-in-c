d5a165beee247c1cc15d541ee4ddd77235a36490
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2025-08-09 00:47:00 +0530

    made the entire neural netowork with calibrated bias and weight with an accuracy of 99.999968 %. plus a new algorithm of analytical gradient descent is used instead of approximation linear differentiation .

--- singleNeuronNetworkWithBias.c
+++ singleNeuronNetworkWithBias.c
@@ -9,7 +9,7 @@
 //  the orign . we have to make the y = mx + c , y intercept to calibrate to 5 .thus the purpose of adding the bias to the model
 //  we will caliberate the bias and the initial weight using a small value to tweak
 float train_data[][2] = {
-    {0, 0},
+    {0, 5},
     {1, 7},
     {2, 9},
     {3, 11},
@@ -45,20 +45,77 @@ int main()
 {
     srand(5000);
     float weight;
-    float rate,bias,output;
-    rate=1e-3;
-    bias=1e-3;
+    float learning_rate,bias,output;
+    bias = 0.1f;
+    learning_rate=0.01f;
     weight = 1.0f + random_weight() * 9.0f;
-    printf("Generated weight : %f ", weight);
+    printf("Generated weight : %f , initial bias : %f ", weight,bias);
 
     printf("\nPredicted values before the training\n");
     for (size_t i = 0; i < train_count; i++)
     {
-        output = train_data[i][0]*weight+bias;
+        output = (train_data[i][0]*weight)+bias;
         printf("input value : %f , expected output : %f , predicted output : %f \n",train_data[i][0],train_data[i][1],output);
     }
-    printf("Initial cost function : %f\n",cost(weight,bias));
+    printf("\n\n\nInitial cost function : %f\n\n\n",cost(weight,bias));
+    float dw,db;
+    float x,y,predicted,error;
+
+    for (size_t i = 0; i < 10000; i++)
+    {
+        dw=0.0f;
+        db=0.0f;
+        for (size_t j = 0; j < train_count; j++)
+        {
+            x=train_data[j][0];
+            y=train_data[j][1];
+            predicted=x*weight+bias;
+            error=y-predicted;
+
+            dw+= -2.0f*error*x;
+            db+= -2.0f*error;
+        }
+
+        dw/=train_count;
+        db/=train_count;
+        weight-=learning_rate*dw;
+        bias-=learning_rate*db;
+        
+        if((i+1)%1000==0){
+            printf("cost function after %d iteration : %f \n",i+1,cost(weight,bias));
+            printf("dw : %f db :%f weight : %f bias : %f \n",dw,db,weight,bias);
+        }
+        
+    }
+    
     
 
+
+
+    printf("\nPredicted values after the training\n");
+    float actual_total=0.0f,predicted_total=0.0f;
+    for (size_t i = 0; i < train_count; i++)
+    {
+        output = (train_data[i][0]*weight)+bias;
+        printf("input value : %f , expected output : %f , predicted output : %f \n",train_data[i][0],train_data[i][1],output);
+        actual_total+=train_data[i][1];
+        predicted_total+=output;
+    }
+    float percent;
+    // percent = (float)(fabs((float)predicted_total- (float)actual_total)/(float)actual_total)*100.0f;
+
+
+
+    percent = (float)fabs((float)(actual_total>predicted_total?predicted_total:actual_total)/(float)(actual_total>predicted_total?actual_total:predicted_total))*100.0f;
+    printf("\n\n accuracy of the model : %f %% \n\n",percent);
+
+    printf("\nweight after training : %f , bias after training  %f \n", weight,bias);
+
+    float input;
+    while(1){
+        printf("\nEnter the input : ");
+        scanf("%f",&input);
+        printf("Input : %f \n Actual expected output : %f \n Predicted input using the model : %f ",input,(float)((2*input)+5),(float)((weight*input)+bias));
+    }
 
 }
